98aa869e1114106a0993aa06346239967b2efa6d
JihoPark01 <75483910+JihoPark01@users.noreply.github.com>
2021-07-21 16:39:34 +0900

    git config --global

--- Assets/NewBehaviourScript1.cs
+++ Assets/NewBehaviourScript1.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class NewBehaviourScript1 : MonoBehaviour
 {
+
+    // Commit & Push Test.
+
     // Start is called before the first frame update
     void Start()
     {
