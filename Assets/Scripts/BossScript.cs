e73e314bd59d56f3d89baa8ad83d24f776587455
조민수 <40791869+eovywjr1@users.noreply.github.com>
2021-07-23 16:46:38 +0900

    210723
    coroutine 관련 수정
    돌진 스크립트 추가(수정필요)

--- Assets/Scripts/BossScript.cs
+++ Assets/Scripts/BossScript.cs
@@ -11,51 +11,88 @@ public class BossScript : MonoBehaviour
     public int moveSpeed;
     public int moveDelaytime;
     public int juniorcallDelaytime;
+    public int dashToplayerDelaytime;
     public bool ismoveDelay;
     public bool isjunorcallDelay;
+    public bool isdashToplayerDelay;
     public Vector2 direction;
+    public Vector2 dashDirection;
     public Rigidbody2D rigidBody;
-    public Transform transform;
+    public Transform transForm;
+    public Transform playerTrasnform; //플레이어 위치 저장 변수(임시)
     public GameObject junior;
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
-        transform = GetComponent<Transform>();
+        transForm = GetComponent<Transform>();
+
         junior = Resources.Load<GameObject>("Prefabs/Junior");
+
         ismoveDelay = true;
         isjunorcallDelay = true;
+        isdashToplayerDelay = true;
+
         hp = 3;
 
         StartCoroutine(RandomMove());
+        StartCoroutine(callJunior());
+        StartCoroutine(dashToplayer());
+
+        
     }
 
     void Update()
     {
-        /*if (!isjunorcallDelay)
+        //잡몹 소환
+        if (!isjunorcallDelay)
         {
-            Debug.Log("1");
-            Instantiate(junior, new Vector2(transform.position.x, transform.position.y),
+            Instantiate(junior, new Vector2(transForm.position.x, transForm.position.y),
                 Quaternion.identity);
+
             isjunorcallDelay = true;
+
+            StartCoroutine(callJunior());
         }
-        else
-            StartCoroutine(callJunior());*/
     }
 
     void FixedUpdate()
     {
+        //보스 이동
         if (!ismoveDelay)
         {
-            //이동 후 딜레이
             rigidBody.AddForce(direction * moveSpeed * Time.deltaTime, ForceMode2D.Impulse);
+
             ismoveDelay = true;
 
             StartCoroutine(RandomMove());
         }
+
+        //보스 돌진
+        if (!isdashToplayerDelay)
+        {
+            playerTrasnform.position = new Vector2(3, 2);
+
+            if (playerTrasnform.position.x >= 0)
+                dashDirection = Vector2.right;
+            else
+                dashDirection = Vector2.left;
+
+            while (transForm.position == playerTrasnform.position)
+            {
+                if (rigidBody.velocity.x < dashSpeed && rigidBody.velocity.x > dashSpeed * (-1))
+                    rigidBody.AddForce(dashDirection * dashSpeed * Time.deltaTime, ForceMode2D.Impulse);
+            }
+
+            rigidBody.velocity = new Vector2(0,0);
+
+            isdashToplayerDelay = true;
+
+            StartCoroutine(dashToplayer());
+        }
     }
 
-    //보스 이동 결정
+    //보스 이동 딜레이
     IEnumerator RandomMove()
     {
         yield return new WaitForSecondsRealtime(moveDelaytime);
@@ -69,12 +106,19 @@ public class BossScript : MonoBehaviour
         ismoveDelay = false;
     }
 
-    //잡몹 소환 결정
+    //잡몹 소환 딜레이
     IEnumerator callJunior()
     {
         yield return new WaitForSecondsRealtime(juniorcallDelaytime);
 
         isjunorcallDelay = false;
-        Debug.Log("2");
+    }
+
+    //플레이어로 돌진 딜레이
+    IEnumerator dashToplayer()
+    {
+        yield return new WaitForSecondsRealtime(dashToplayerDelaytime);
+
+        isdashToplayerDelay = false;
     }
 }
