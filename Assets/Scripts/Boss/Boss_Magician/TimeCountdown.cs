d15ee4b7cd473cb5d7f1b2743771bc0f02a842d7
KMKIH <86542540+KMKIH@users.noreply.github.com>
2021-08-03 15:11:28 +0900

    0803
    성 내부 에셋 다운로드 및 임포트
    마법사형 보스 맵 디자인
    카메라 움직임 관련 스크립트 작성
    마법사형 보스 스킬 프리팹 크기 및 범위 조정
    마법사형 보스 횃불, 타이머 관련 스크립트 수정

---
+++ Assets/Scripts/Boss/Boss_Magician/TimeCountdown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCountdown : MonoBehaviour
+{
+    public Text TimeCount;
+    public float TimeCost;
+
+    public bool TimeEnd;
+
+    private void Awake()
+    {
+        TimeEnd = false;
+    }
+
+    private void Update()
+    {
+        TimeCost -= Time.deltaTime;
+        TimeCount.text = $"{TimeCost:N2}";
+        if (TimeCost <= 0.1)
+        {
+            TimeEnd = true;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Temp/CameraMoving.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoving : MonoBehaviour
+{
+    public GameObject target; // 카메라가 따라갈 대상
+    public float moveSpeed; // 카메라가 따라갈 속도
+    private Vector3 targetPosition; // 대상의 현재 위치
+
+    void Update()
+    {
+        // 대상이 있는지 체크
+        if (target.gameObject != null)
+        {
+            // this는 카메라를 의미 (z값은 카메라값을 그대로 유지)
+            targetPosition.Set(target.transform.position.x, this.transform.position.y, this.transform.position.z);
+            if (targetPosition.x < -22)
+                targetPosition.x = -22;
+            else if (targetPosition.x > 22)
+                targetPosition.x = 22;
+
+            // vectorA -> B까지 T의 속도로 이동
+                this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+    }
+}
