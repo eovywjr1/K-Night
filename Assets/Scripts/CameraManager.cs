ad8ef45c5f12c70516cd62af88278066bd76d1c3
JihoPark01 <75483910+JihoPark01@users.noreply.github.com>
2021-08-08 12:03:02 +0900

    0808: Village 씬 생성 및 UI 90%(?) 완료
    -ESC 메뉴는 노션에 올려둔 시안3을 일단 적용해두었습니다.
    
    -마을 씬 7개 생성 및 복사 완료했습니다.
    [마을 씬에 아직 미구현된 것]
    Village_Present: 폐허 남음
    Village_Present_AfterTutorial: 폐허 남음
    Village_Past_BeforeCombat: 혼령 배치 남음
    Village_Past_AfterCombat: 혼령 배치 남음, Yes or No UI 아직 미구현
    Village_Sorcerer: 혼령 배치 남음, 오두막을 모닥불로 대체해 둔 상태
    Village_(First/Second)Ending: 왕 배치 남음, 대사 자동시작 기능 아직 미구현
    
    -UI: 업적 변수 저장 및 불러오기 남음
    -플레이어 체력바는 hp 설정이 끝난 후 구현
    
    ***회의 때: 혼령(사람 형태) 이미지와 추가적인 왕 이미지(혼령이 왕의 모습으로 변했을 때) 아트 에셋 부탁드리겠습니다.
    ***플레이어의 맵 이동을 처리하는 방식?

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,20 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraManager : MonoBehaviour
 {
     public Player player;
 
     void Start()
-    {
+    {        
         player = FindObjectOfType<Player>();
         this.gameObject.transform.position = new Vector3(player.transform.position.x, this.gameObject.transform.position.y, -1);
     }
 
     void Update()
     {
-        if (player.transform.position.x > -22 && this.gameObject.transform.position.x < 18)
+        // 
+        if (ReturnIsInVillage())
+        {
             this.gameObject.transform.position = new Vector3(player.transform.position.x, this.gameObject.transform.position.y, -1);
+
+        }
+        else
+        {
+            if (player.transform.position.x > -22 && this.gameObject.transform.position.x < 18)
+                this.gameObject.transform.position = new Vector3(player.transform.position.x, this.gameObject.transform.position.y, -1);
+        }
+
+
+
+
+
+    }
+
+
+
+    private bool ReturnIsInVillage()
+    {
+        string[] playerPlace = SceneManager.GetActiveScene().name.Split('_');
+
+        if(playerPlace[0] == "Village")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
     }
 }
--- Assets/Scripts/TalkManager.cs
+++ Assets/Scripts/TalkManager.cs
@@ -14,13 +14,16 @@ public class TalkManager : MonoBehaviour
     bool talkIsActive;
     public int talkIndex;
 
+    // ESC.
     bool escMenuPanelIsActive;
-
     public GameObject escMenuPanel;
+    public Text nextQuestText;
 
+    // 대사 딕셔너리.
     Dictionary<int, string[]> talkData;
 
-    public GameObject firstTalkingNPCInStory; // ?!
+    public int lastTalkID;
+
 
     private void Awake()
     {
@@ -28,6 +31,9 @@ public class TalkManager : MonoBehaviour
         escMenuPanelIsActive = false;
         talkData = new Dictionary<int, string[]>();
         GenerateTalkData();
+
+        // For nextQuest.
+        lastTalkID = 0;
     }
 
     private void Start()
@@ -39,6 +45,8 @@ public class TalkManager : MonoBehaviour
 
     private void Update()
     {
+        TextQuestInEscAndTriggerStoryEvent();
+
         if (Input.GetKeyDown("escape"))
         {
             ActivateEscMenuPanel();
@@ -228,6 +236,8 @@ public class TalkManager : MonoBehaviour
         ObjTalkData objTalkData = ObjectForTalkId.GetComponent<ObjTalkData>();
         Talk(objTalkData.talkId);
 
+        SetLastTalkID(objTalkData.talkId);
+
         talkPanel.SetActive(talkIsActive);
         
     }
@@ -251,6 +261,58 @@ public class TalkManager : MonoBehaviour
             escMenuPanel.SetActive(false);
         }
 
+    }
+
+    
+    private void SetLastTalkID(int id)
+    {
+        if(id > lastTalkID)
+        {
+            lastTalkID = id;
+        }
+
+    }
+
+   
+    private void TextQuestInEscAndTriggerStoryEvent()
+    {
+        if(lastTalkID == 0)
+        {
+            nextQuestText.text = " ";
+        }
+        if(lastTalkID == 100)
+        {
+            nextQuestText.text = "마을 서쪽에 나타난 괴물을 처치하자!";
+        }
+        if(lastTalkID == 200)
+        {
+            nextQuestText.text = "마을 동쪽의 동굴을 통해 과거로 가서, 괴물의 원한을 잠재우자!";
+            RemoveCaveBarricade();
+        }
+        if(lastTalkID == 300)
+        {
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+    // 스토리 상의 함수. (npc 움직임, 오브젝트 제거 or 변경 등..)
+    public void RemoveCaveBarricade()
+    {
+        GameObject caveBarricade = GameObject.Find("CaveBarricade");
+        if (caveBarricade != null)
+        {
+            Destroy(caveBarricade);
+        }
     }
 
 }
