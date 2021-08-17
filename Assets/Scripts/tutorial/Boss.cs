22f5626f1dea43120f54632d792f8bb0c8303547
KMKIH <86542540+KMKIH@users.noreply.github.com>
2021-08-17 17:07:55 +0900

    Revert "Merge branch 'main' of https://github.com/eovywjr1/K-Night into main"
    This reverts commit 624914900421a3ddc1f0e3626ad154157a2ee50c, reversing
    changes made to 4a1e18bb9765cc21a84a01e7266c733c1d49d701.

--- Assets/Scripts/tutorial/Boss.cs
+++ Assets/Scripts/tutorial/Boss.cs
@@ -2,23 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boss : TutorialMonsterBase
+public class Boss : MonoBehaviour
 {
-    int dashSpeed = 3;
-    int skillDelaytime = 5;
+    public int hp;
+    public int power;
+    public int dashSpeed = 3;
+    public int moveSpeed = 300;
+    public int skillDelaytime = 5;
 
-    bool isjunorcallDelay = true;
-    bool isdashToplayerDelay = true;
-    bool isattack = false;
+    public bool isjunorcallDelay = true;
+    public bool isdashToplayerDelay = true;
+    public bool ismoveDelay = true;
+    public bool isattack = false;
+
+    public Vector2 direction;
+    Vector3 playerPosition;
+
+    Rigidbody2D rigidBody;
+    SpriteRenderer spriteRenderer;
+    BoxCollider2D boxCollider;
 
     public GameObject junior;
     GameObject childBoxCollider;
+    Player player;
+
+    RaycastHit2D rayHit;
 
     void Start()
     {
+        player = FindObjectOfType<Player>();
         childBoxCollider = this.gameObject.transform.GetChild(0).gameObject;
 
-        Initial();
+        rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
 
         StartCoroutine(Skilldelay());
     }
@@ -28,24 +45,28 @@ public class Boss : TutorialMonsterBase
         if (!isjunorcallDelay)
             JuniorCall();
 
+        //죽었을 때
+        if (hp <= 0)
+            this.gameObject.SetActive(false);
+
         if (!isdashToplayerDelay)
             DashFindCollision();
     }
 
     void FixedUpdate()
     {
-        if (!isMoveDelay)
-            RandomMove();
+        if (!ismoveDelay)
+            Move();
 
         if (!isdashToplayerDelay)
             Dash();
     }
     
     //보스 이동 함수
-    void RandomMove()
+    void Move()
     {
-        rigidBody.AddForce(direction * 300 * Time.deltaTime, ForceMode2D.Impulse);
-        isMoveDelay = true;
+        rigidBody.AddForce(direction * moveSpeed * Time.deltaTime, ForceMode2D.Impulse);
+        ismoveDelay = true;
         StartCoroutine(Skilldelay());
     }
 
@@ -57,15 +78,23 @@ public class Boss : TutorialMonsterBase
         StartCoroutine(Skilldelay());
     }
 
+    //공격받았을 때
+    public void Ondamaged(int power)
+    {
+        hp -= power;
+    }
+
     void Dash()
     {
         //자식 오브젝트 레이어 변경
         childBoxCollider.layer = 9;
 
         //위치까지 속도 추가
-        if ((direction == Vector2.right && playerPosition.x > this.gameObject.transform.position.x) || (direction == Vector2.left && playerPosition.x < this.gameObject.transform.position.x))
-            Move(10, dashSpeed);
-
+        if ((direction == Vector2.right && playerPosition.x > this.gameObject.transform.position.x) || (direction == Vector2.left && playerPosition.x < this.gameObject.transform.position.x)) {
+            //속도 제한
+            if ((direction == Vector2.right && rigidBody.velocity.x < dashSpeed) || (direction == Vector2.left && rigidBody.velocity.x > dashSpeed * (-1)))
+                rigidBody.AddForce(direction * 10 * Time.deltaTime, ForceMode2D.Impulse);
+        }
         //위치 도착 후
         else
         {
@@ -89,9 +118,9 @@ public class Boss : TutorialMonsterBase
     void DashFindCollision()
     {
         Debug.DrawRay(this.gameObject.transform.position, direction * 1.6f, new Color(0, 0, 1), LayerMask.GetMask("Player"));
-        raycastHit = Physics2D.Raycast(this.gameObject.transform.position, direction, 1.6f, LayerMask.GetMask("Player"));
+        rayHit = Physics2D.Raycast(this.gameObject.transform.position, direction, 1.6f, LayerMask.GetMask("Player"));
 
-        if (raycastHit.collider.name == "Player" && !isattack)
+        if (rayHit.collider.name == "Player" && !isattack)
         {
             isattack = true;
 
@@ -109,17 +138,31 @@ public class Boss : TutorialMonsterBase
             //잡몹 소환
             case 1:
                 isjunorcallDelay = false;
-
+                Debug.Log("1");
                 break;
 
             //대쉬
             case 2:
-                PlayerPositionSave();
+                //플레이어 위치 및 방향 저장
+                playerPosition = new Vector3(player.GetTransform().position.x, this.gameObject.transform.position.y);
+                if (playerPosition.x >= this.transform.position.x)
+                {
+                    direction = Vector2.right;
+
+                    spriteRenderer.flipX = true;
+                }
+                else
+                {
+                    direction = Vector2.left;
+
+                    spriteRenderer.flipX = false;
+                }
 
                 this.gameObject.layer = 9;
 
                 isdashToplayerDelay = false;
 
+                Debug.Log("2");
                 break;
 
             //랜덤 이동
@@ -138,8 +181,9 @@ public class Boss : TutorialMonsterBase
                     spriteRenderer.flipX = true;
                 }
 
-                isMoveDelay = false;
+                ismoveDelay = false;
 
+                Debug.Log("3");
                 break;
 
             default:
