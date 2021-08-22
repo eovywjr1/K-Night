c796b655080e70c7b3c5721d5b8813fef68aa138
KMKIH <86542540+KMKIH@users.noreply.github.com>
2021-08-22 17:56:25 +0900

    0822_2
    스파이더 보스 조정

--- Assets/Scripts/Boss/Boss_Spider/Boss_Spider.cs
+++ Assets/Scripts/Boss/Boss_Spider/Boss_Spider.cs
@@ -1,65 +1,51 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Boss_Spider: Boss_form
 {
-
-    //보스의 공격 딜레이
-    private float attackDelay; //공격 딜레이
-    public float attackDelay_Dash; //대쉬공격 딜레이
-    public float attackDelay_Throw; //던지기공격 딜레이
-    private bool canAttack_Dash; //공격 가능 여부(스킬 쿨타임)
-    private bool canAttack_Throw; //공격 가능 여부(스킬 쿨타임)
-    private float lastAttackTime_Dash; //마지막 공격 시점
-    private float lastAttackTime_Throw; //마지막 공격 시점
 
     Vector3 startPoint;
     protected override void Start()
     {
         base.Start();
         //dead = true;
-        inRange = false;
-        lastAttackTime_Throw = 0f;
+        doDash = false;
+
         Skills();
-        Invoke(nameof(Skills), attackDelay);
     }
-
     //스킬 사용
-    void Skills() 
+    void Skills()
     {
-        //쿨타임 여부(Dash)
-        if (lastAttackTime_Dash + attackDelay_Dash <= Time.time)
-        {
-            canAttack_Dash = true;
-            lastAttackTime_Dash = Time.time;
-        }
-        else canAttack_Dash = false;
-        //쿨타임 여부(Throw)
-        if (lastAttackTime_Throw + attackDelay_Throw <= Time.time)
-        {
-            canAttack_Throw = true;
-            lastAttackTime_Throw = Time.time;
-        }
-        else canAttack_Throw = false;
-
         //좌우 확인
         FindPlayer();
-
-        //범위 체크
-        if (inRange && canAttack_Dash && !doDash)
+        if (inRange)
         {
             startPoint = transform.position;
-            doDash = true;
-            Dash(dashSpeed);
-            attackDelay = attackDelay_Dash;
+            //뒤로 잠시 이동 후 대쉬
+            StartCoroutine(UseDash());
         }
-        else if(!inRange && canAttack_Throw)
+        else if (!inRange)
         {
-            ThrowStones ();
+            ThrowStones();
             Invoke(nameof(ThrowStones), 0.5f);
             Invoke(nameof(ThrowStones), 1f);
-            attackDelay = attackDelay_Throw;
+            Debug.Log("다 던졌어");
+            StartCoroutine(AttackEnd(1.5f));
         }
-        Invoke(nameof(Skills),attackDelay);
+    }
+    IEnumerator UseDash()
+    {
+        rigid.AddForce(direction * -4, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(3f);
+        doDash = true;
+        Dash(dashSpeed);
+        Skills();
+    }
+    IEnumerator AttackEnd(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Skills();
     }
     private void FixedUpdate()
     {
@@ -90,11 +76,13 @@ public class Boss_Spider: Boss_form
             Debug.Log("대쉬 맞음");
             player.HpDecrease(damage_Dash);
         }
+
+        /* 몸빵데미지
         else if (collision.gameObject.CompareTag("Player") && !doDash)
         {
             Debug.Log("몸빵 맞음");
             player.HpDecrease(damage_Touch);
-        }
+        }*/
     }
 
     //데미지 받기
